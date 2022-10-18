07a056542ef56d35d00e6f05560268ba839e1431
Anton <62949848+icepaq@users.noreply.github.com>
2022-10-18 09:47:46 -0400

    create book data helper, center featured book

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -3,6 +3,7 @@ import { addBook } from "../app/counterSlice";
 import { useSelector, useDispatch } from "react-redux";
 import store from "../app/store";
 import { Provider } from "react-redux";
+import books from "../helpers/books";
 
 export default function HomeWrapper() {
   const dispatch = useDispatch();
@@ -13,14 +14,16 @@ export default function HomeWrapper() {
       <div className="w-fit my-0 mx-auto text-xl font-bold pt-[30px]">
         Featured
       </div>
-      <Book
-        click={() => dispatch(addBook({}))}
-        title={"Diary of a Wimpy Kid"}
-        description={
-          "Greg Heffley and his family hit the road for a cross-country camping trip, ready for the adventure of a lifetime."
-        }
-        image={"/diary.jpg"}
-      />
+      <div className="flex flex-row flex-wrap justify-center">
+        <Book
+          click={() => dispatch(addBook({}))}
+          title={"Diary of a Wimpy Kid"}
+          description={
+            "Greg Heffley and his family hit the road for a cross-country camping trip, ready for the adventure of a lifetime."
+          }
+          image={"/diary.jpg"}
+        />
+      </div>
 
       <div className="w-fit my-0 mx-auto text-xl font-bold pt-[30px]">
         Other Selections
@@ -39,33 +42,3 @@ export default function HomeWrapper() {
     </div>
   );
 }
-
-const books = [
-  {
-    title: "Diary of a Wimpy Kid",
-    description:
-      "Greg Heffley and his family hit the road for a cross-country camping trip, ready for the adventure of a lifetime.",
-    image: "/diary.jpg",
-  },
-
-  {
-    title: "Never Split the Difference",
-    description:
-      "Chriss Voss, an ex-FBI hostage negotiator, shares his strategies for success in business and personal negotiations.",
-    image: "/neversplit.jpg",
-  },
-
-  {
-    title: "Rich Dad Poor Dad",
-    description:
-      "What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!",
-    image: "/richdad.jpg",
-  },
-
-  {
-    title: "Way of the Wolf",
-    description:
-      "Jordan Belfort reveals the step-by-step sales and persuasion system proven to turn anyone into a sales-closing, money-earning rock star.",
-    image: "/wolf.jpg",
-  },
-];
---
+++ src/helpers/books.ts
@@ -0,0 +1,31 @@
+const books = [
+  {
+    title: "Diary of a Wimpy Kid",
+    description:
+      "Greg Heffley and his family hit the road for a cross-country camping trip, ready for the adventure of a lifetime.",
+    image: "/diary.jpg",
+  },
+
+  {
+    title: "Never Split the Difference",
+    description:
+      "Chriss Voss, an ex-FBI hostage negotiator, shares his strategies for success in business and personal negotiations.",
+    image: "/neversplit.jpg",
+  },
+
+  {
+    title: "Rich Dad Poor Dad",
+    description:
+      "What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!",
+    image: "/richdad.jpg",
+  },
+
+  {
+    title: "Way of the Wolf",
+    description:
+      "Jordan Belfort reveals the step-by-step sales and persuasion system proven to turn anyone into a sales-closing, money-earning rock star.",
+    image: "/wolf.jpg",
+  },
+];
+
+export default books;
