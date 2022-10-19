93c1389d9b6faf5a95a23c367dfbb05271627e92
Anton <62949848+icepaq@users.noreply.github.com>
2022-10-18 21:01:11 -0400

    add to cart by quantity

--- src/app/BookPage.tsx
+++ src/app/BookPage.tsx
@@ -2,34 +2,55 @@ import { useDispatch } from "react-redux";
 import { incrementByAmount, addBook } from "./counterSlice";
 import book from "../helpers/books";
 import Swal from "sweetalert2";
+import { useState } from "react";
 
 const BookPage = ({ bookId }: any) => {
-
   const dispatch = useDispatch();
+
+  const [quantity, setQuantity] = useState(1);
 
   return (
     <div className="m-10">
-      <div className="flex flex-row">
-        <div className="w-[300px] h-auto">
-          <img src={book[bookId].image} alt="" />
-        </div>
+      <div className="flex flex-row justify-around">
         <div className="w-[500px] h-auto m-3">
           <h1 className="text-2xl font-bold">{book[bookId].title}</h1>
           <p className="">{book[bookId].description}</p>
-          <p className="text-lg mt-[70px]">Price: ${book[bookId].price}</p>
+        </div>
+        <div className="w-[200px] h-auto">
+          <img src={book[bookId].image} alt="" />
+          <div>
+            <p className="text-lg mt-[70px]">Price: ${book[bookId].price}</p>
+            Quantity
+            <select
+              name="cars"
+              id="cars"
+              defaultValue={quantity}
+              onChange={(e) => {
+                setQuantity(Number.parseInt(e.target.value));
+              }}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <div
+              onClick={() => {
+                for (let i = 0; i < quantity; i++) {
+                  dispatch(incrementByAmount(book[bookId].price)); // increment the total price
+                  dispatch(addBook(book[bookId])); // add the book to the cart
+                }
 
-          <div
-            onClick={() => {
-              dispatch(incrementByAmount(book[bookId].price)); // increment the total price
-              dispatch(addBook(book[bookId])); // add the book to the cart
-              Swal.fire({
-                title: "Added to cart!",
-                icon: "success",
-              })
-            }}
-            className="mt-2 px-[25px] py-[10px] bg-blue-300 w-fit cursor-pointer"
-          >
-            Add to Cart
+                Swal.fire({
+                  title: "Added to cart!",
+                  icon: "success",
+                });
+              }}
+              className="mt-2 px-[25px] py-[10px] bg-blue-300 w-fit cursor-pointer"
+            >
+              Add to Cart
+            </div>
           </div>
         </div>
       </div>
